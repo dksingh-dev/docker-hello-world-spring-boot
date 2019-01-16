fa9ec7353ddda5b4e481d3cc9b43e1447d566aa8
dksingh-dev <dksingh.dev@gmail.com>
2019-01-15 21:13:16 -0800

    test

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World From New\n";
+        return "Hello World From \n";
     }
     
 }
